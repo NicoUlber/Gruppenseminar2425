82802041043684a448a8a264d11f31e65fd9fa1e
Nico Ulber <ulber@iftc.uni-hannover.de>
2024-11-29 14:09:33 +0100

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Gruppenseminar2425 {
+  partial class Form1 {
+    /// <summary>
+    /// Erforderliche Designervariable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Verwendete Ressourcen bereinigen.
+    /// </summary>
+    /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Vom Windows Form-Designer generierter Code
+
+    /// <summary>
+    /// Erforderliche Methode für die Designerunterstützung.
+    /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+    /// </summary>
+    private void InitializeComponent() {
+      System.Windows.Forms.Button buttonTutorial;
+      this.buttonOpenFile = new System.Windows.Forms.Button();
+      buttonTutorial = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // buttonOpenFile
+      // 
+      this.buttonOpenFile.Location = new System.Drawing.Point(30, 17);
+      this.buttonOpenFile.Name = "buttonOpenFile";
+      this.buttonOpenFile.Size = new System.Drawing.Size(177, 54);
+      this.buttonOpenFile.TabIndex = 0;
+      this.buttonOpenFile.Text = "button1";
+      this.buttonOpenFile.UseVisualStyleBackColor = true;
+      this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+      // 
+      // buttonTutorial
+      // 
+      buttonTutorial.Location = new System.Drawing.Point(620, 23);
+      buttonTutorial.Name = "buttonTutorial";
+      buttonTutorial.Size = new System.Drawing.Size(153, 47);
+      buttonTutorial.TabIndex = 1;
+      buttonTutorial.Text = "Tutorial-Button";
+      buttonTutorial.UseVisualStyleBackColor = true;
+      buttonTutorial.Click += new System.EventHandler(this.buttonTutorial_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(buttonTutorial);
+      this.Controls.Add(this.buttonOpenFile);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpenFile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PatricksImagingTools;
+
+
+namespace Gruppenseminar2425 {
+  public partial class Form1 : Form {
+    public Form1() {
+      InitializeComponent();
+    }
+    Bitmap bitBeispiel; //Bitmaps sind unsere "klassischen" Bilder, also eine Fläche mit ganz vielen Pixeln, die alle 2 Koordinaten haben und einen Farbwert
+    ImageObject ioBeispiel; //ImageObjects sind Objekte, die eigentlich nur sehr lange Listen mit Pixeln sind
+    ImageObjectList iol_cellpose_result;
+
+
+    //Der folgende Button öffnet zunächst die Tiff-Ausgabe von cellpose (das sind 16bit Graustufenbilder, die sind blöd einzulesen)
+    private void buttonOpenFile_Click(object sender, EventArgs e) { //alles was jetzt folgt, passiert, sobald dieser button gedrückt wird
+      OpenFileDialog openFileDialog = new OpenFileDialog(); //öffnet den Windowseigenen "Datei-öffnen"-Dialog
+      DialogResult result = openFileDialog.ShowDialog();
+      if (result == DialogResult.OK) {//falls das fenster nicht weggeklickt wurde etc.
+        string path = openFileDialog.FileName; //FileName ist der pfad, der zu der Datei führt (zB D:/Eigene Dateien/Bild.tif)
+        ImageMagick.MagickImage magickImage = new ImageMagick.MagickImage(path);
+        Bitmap bitmap = new Bitmap(path);
+        ushort[] pixelvalues = magickImage.GetPixels().GetValues();
+        iol_cellpose_result = new ImageObjectList();
+        List<ushort> shorts  = new List<ushort>();
+
+        for (int y = 0; y < bitmap.Height; y++) {
+          for (int x = 0; x < bitmap.Width; x++) {
+            ushort p=pixelvalues[y*bitmap.Width+x];
+            if (p!=0) {
+              if (shorts.Contains(p)) {
+                iol_cellpose_result[shorts.IndexOf(p)].Add(new Point(x, y));
+              }
+              else {
+                iol_cellpose_result.Add(new ImageObject());
+                shorts.Add(p);
+                iol_cellpose_result[shorts.IndexOf(p)].Add(new Point(x, y));
+              }
+            }
+          }
+        }
+
+
+      }
+
+      
+    }
+
+    private void buttonTutorial_Click(object sender, EventArgs e) {
+      //so öffnet man "normale" 8-bit Bilder:
+      OpenFileDialog openFileDialog = new OpenFileDialog(); //erstmal wieder die Datei auswählen
+      DialogResult result = openFileDialog.ShowDialog();
+      if (result == DialogResult.OK) {
+        bitBeispiel=new Bitmap(openFileDialog.FileName);
+        
+      }
+    }
+  }
+}
